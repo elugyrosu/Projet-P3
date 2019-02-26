ebc2e3ac1f886ab4e546f08d1ef506fe079c9292
leeloomulti <leeloomulti@gmail.com>
2019-02-26 13:15:54 +0100

    Ajout fonction chronomêtre, validation mentor, corrections fonction chooseEnemy

--- ProjetP3/Game/Game.swift
+++ ProjetP3/Game/Game.swift
@@ -12,6 +12,17 @@ class Game {
     let teamFactory = TeamFactory()
     var teams = [Team]()
     var roundCount = 0
+    var startCounter: Date?
+    
+    func showTimer(startBattleDate: Date) -> String{
+        let interval = Date().timeIntervalSince(startBattleDate as Date)
+        let formatter = DateComponentsFormatter()
+        formatter.unitsStyle = .short
+        guard let timer = formatter.string(from: interval) else {
+            return ""
+        }
+        return timer
+    }
     
     func start(){
         print("===============================")
@@ -19,10 +30,11 @@ class Game {
         print("===============================")
         teamFactory.createTeams()
         teams = teamFactory.teams
-        Battle()
+        battle()
     }
     
-    func Battle(){
+    func battle(){
+        startCounter = Date()
         repeat {
             roundCount += 1
             for i in 0..<teams.count{   
@@ -34,20 +46,6 @@ class Game {
                     let heroSelected = chooseCharacter(team: team)
                     randomTreasure(heroSelected: heroSelected)
                     
-                    func chooseEnemy(){                                     // function directly created in Battle() to improve visibility (to much properties)
-                        teamNamePrint(team: team)                           // improved for a two player Game, need to be change on a team selection interface if more players
-                        print("choose the ENEMY you want to ATTACK")
-                        print("")
-                        if i == 0{
-                            let teamEnemy = teamFactory.teams[i+1]
-                            teamAttack(teamEnemy: teamEnemy, heroSelected: heroSelected, team: team)
-                            
-                        }else{
-                            let teamEnemy = teamFactory.teams[i-1]
-                            teamAttack(teamEnemy: teamEnemy, heroSelected: heroSelected, team: team)
-                        }
-                    }
-                    
                     if let magus = heroSelected as? Magus{                     // if character selected is a Magus you can treat all characters of your team who are not dead
                         if team.isCharacterAlone() == false{
                             teamNamePrint(team: team)
@@ -55,8 +53,12 @@ class Game {
                             print("")
                             let heroTreat = chooseCharacter(team: team)
                             magus.treat(treatedHero: heroTreat)
-                        }else{ chooseEnemy()}                                     // if Magus is alone, he can just attack with his specific weapon (battle balance, in case of 1 Magus vs 1 Magus)}
-                    }else {chooseEnemy()}                  // if Character is other type: Attack
+                        }else{
+                            chooseEnemy(team: team, heroSelected: heroSelected, index: i)  // if Magus is alone, he can just attack with his specific weapon (battle balance, in case of 1 Magus vs 1 Magus)}
+                        }
+                    }else {
+                        chooseEnemy(team: team, heroSelected: heroSelected, index: i)   // if Character is other type: Attack
+                    }
                 }
             }
         } while teamFactory.isEnd() == false // 1 team = end of the battle
@@ -111,15 +113,30 @@ class Game {
             if teamEnemy.isTeamDefeat() == true{
                 if teams.isEnd() == true{
                     print("Congratulation \(teamPlayerName) win the party in \(roundCount) rounds !")
-                    
+                    guard let startCounter = startCounter else {return}
+                    print("game duration: " + showTimer(startBattleDate: startCounter))
                 }
             }
+        }
+    }
+    
+    func chooseEnemy(team: Team, heroSelected: Character, index: Int){                                     // function directly created in Battle() to improve visibility (to much properties)
+        teamNamePrint(team: team)                           // improved for a two player Game, need to be change on a team selection interface if more players
+        print("choose the ENEMY you want to ATTACK")
+        print("")
+        if index == 0{
+            let teamEnemy = teamFactory.teams[index+1]
+            teamAttack(teamEnemy: teamEnemy, heroSelected: heroSelected, team: team)
+            
+        }else{
+            let teamEnemy = teamFactory.teams[index-1]
+            teamAttack(teamEnemy: teamEnemy, heroSelected: heroSelected, team: team)
         }
     }
 
     func randomTreasure(heroSelected: Character) { // random appear chest, scepter for magus, random weapon attack for others
         
-        let treasure = arc4random_uniform(100) // chest appear 1/5 time, better than Bool.random()
+        let treasure = Int.random(in: 0...100)  // chest appear 1/5 time, better than Bool.random()    older form: let treasure = arc4random_uniform(100)
         if treasure <= 20 {
             print("Surprise ! a chest appear")
             if let magus = heroSelected as? Magus{
